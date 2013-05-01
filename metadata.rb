9356b71693c42e0d24207ff4681c30df35044bee
Remy Loubradou <remyloubradou@gmail.com>
2013-05-01 12:17:52 +0100

    RELEASE: v0.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "adam@hipsnip.com/remy@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs/Configures Jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "0.5.0"
 supports 'ubuntu', ">= 12.04"
 
 depends "java", "1.10.0"
