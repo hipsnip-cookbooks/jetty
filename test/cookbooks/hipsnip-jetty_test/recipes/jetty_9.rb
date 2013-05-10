957bdd11cf93e4859254c46f31db28ee0bf0dd5b
Remy Loubradou <remyloubradou@gmail.com>
2013-05-10 13:52:01 +0100

    NEW: Test new stable release of Jetty 9.

--- test/cookbooks/hipsnip-jetty_test/recipes/jetty_9.rb
+++ test/cookbooks/hipsnip-jetty_test/recipes/jetty_9.rb
@@ -1,8 +1,8 @@
 node.set['java']['jdk_version'] = 7
 
 node.set['jetty']['port'] = 8080
-node.set['jetty']['version'] = '9.0.2.v20130417'
-node.set['jetty']['link'] = 'http://eclipse.org/downloads/download.php?file=/jetty/stable-9/dist/jetty-distribution-9.0.2.v20130417.tar.gz&r=1'
-node.set['jetty']['checksum'] = '6ab0c0ba4ff98bfc7399a82a96a047fcd2161ae46622e36a3552ecf10b9cddb9'
+node.set['jetty']['version'] = '9.0.3.v20130506'
+node.set['jetty']['link'] = 'http://eclipse.org/downloads/download.php?file=/jetty/9.0.3.v20130506/dist/jetty-distribution-9.0.3.v20130506.tar.gz&r=1'
+node.set['jetty']['checksum'] = 'eff8c9c63883cae04cec82aca01640411a6f8804971932cd477be2f98f90a6c4'
 
 include_recipe 'hipsnip-jetty'
